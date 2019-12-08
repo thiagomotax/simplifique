17ec66198de5c2ddbe376a0a62886f3ed51c2aa7
Nuri-an <50783727+Nuri-an@users.noreply.github.com>
2019-12-08 09:31:23 -0300

    Add files via upload

--- controller/ControllerLogado.php
+++ controller/ControllerLogado.php
@@ -4,12 +4,15 @@ $acao = filter_var($_POST["acao"], FILTER_SANITIZE_STRING);
 
 switch ($acao) {
     case 'editar':
-        atualizarLogado();
+        atualizarDados();
+        break;
+    case 'foto':
+        atualizarFoto();
         break;
 }
 
 
-function atualizarLogado() {
+function atualizarDados() {
     require_once ('../model/ModelLogado.php');
     require_once ('../dao/DaoLogado.php');
     $dao = new DaoLogado();
@@ -18,14 +21,12 @@ function atualizarLogado() {
    
     $id = filter_var($_POST["id"], FILTER_SANITIZE_NUMBER_INT);
     $nome = filter_var($_POST["nome"], FILTER_SANITIZE_STRING);
-    $cpf = filter_var($_POST["cpf"], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST["email"], FILTER_SANITIZE_STRING);
 
 
     $Logado = new ModelLogado();
     $Logado->setIdLogado($id);
     $Logado->setNomeLogado($nome);
-    $Logado->setCpfLogado($cpf);
     $Logado->setEmailLogado($email);
 
     $dao->atualizarDados($Logado);
@@ -41,12 +42,53 @@ function atualizarLogado() {
     $Logado->setIdLogado($id);
     $Logado->setSenhaLogado($senha);
 
+    $dao->atualizarSenha($Logado);
+
+
     }
-
-
-
-
+    
 }
 
+
+
+function atualizarFoto() {
+    require_once ('../model/ModelLogado.php');
+    require_once ('../dao/DaoLogado.php');
+    $dao = new DaoLogado();
+
+
+    $id = filter_var($_POST["id"], FILTER_SANITIZE_NUMBER_INT);
+    $foto = filter_var($_POST["foto"], FILTER_SANITIZE_STRING);
+
+    //print_r($_FILES);
+
+    //if(isset($_FILES['foto']) && $_FILES['foto']['error'] == 0){
+    
+    $fileTemp=$_FILES['foto']['tmp_name'];
+    //$fileName=$_FILES['foto']['name'];
+    $fileType=$_FILES['foto']['type'];
+    
+    $filenameTemp= explode('.', $foto);
+    $fileExtension= end( $filenameTemp );
+    
+    $newFileName= 'perfil_' . time() . '.' . $fileExtension;
+    
+ /* Insira aqui a pasta que deseja salvar o arquivo*/
+    $uploadir='../assets/media/avatars/';
+    
+    $destino= $uploadir . $fileName;
+   
+    move_uploaded_file($fileTemp, $destino);
+
+
+    $Logado = new ModelLogado();
+    $Logado->setIdLogado($id);
+    $Logado->setFotoLogado($newFileName);
+
+    $dao->atualizarFoto($Logado);
+    //}
+    header(sprintf('location: %s', $_SERVER['HTTP_REFERER']));
+
+    }
 
 ?>
